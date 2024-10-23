813646f9f0d8b7866af6ccb965212374af4e7e8b
AzmainAzad <nafiazmain2024@gmail.com>
2024-10-23 22:07:11 +0600

    Solving the minus figure and NaN input problem

--- JS/features.js
+++ JS/features.js
@@ -1,14 +1,14 @@
+// Starting of Functions for showing the donation balance 
 document.getElementById('btn-add-money')
    .addEventListener('click', function(){
            event.preventDefault();
 
            const addMoney = getInputFieldValueById('input-add-money');
-           
-           if(isNaN(addMoney)){
-                alert('Failed to add Money');
-                return;
-           }
 
+           if(isNaN(addMoney)){
+                alert('Failed to add money');
+           }
+         
            if(addMoney > 0){
                 const balance = getTextFieldValueById('account-balance');
 
@@ -30,6 +30,10 @@ document.getElementById('btn-add-money')
            event.preventDefault();
 
            const addMoney = getInputFieldValueById('feni-input-add-money');
+
+           if(isNaN(addMoney)){
+                alert('Failed to add money');
+           }
            
            if(addMoney > 0){
                 const balance = getTextFieldValueById('feni-account-balance');
@@ -51,6 +55,10 @@ document.getElementById('btn-add-money')
            event.preventDefault();
 
            const addMoney = getInputFieldValueById('quota-input-add-money');
+
+           if(isNaN(addMoney)){
+                alert('Failed to add money');
+           }
            
            if(addMoney > 0){
                 const balance = getTextFieldValueById('quota-account-balance');
@@ -68,11 +76,25 @@ document.getElementById('btn-add-money')
            
    }); 
 
+// Ending of Functions for showing the donation balance 
+
+
+
+
+// Starting of Functions for showing the main balance
    document.getElementById('btn-add-money')
    .addEventListener('click', function(){
            event.preventDefault();
 
            const addMoney = getInputFieldValueById('input-add-money');
+
+           if(isNaN(addMoney)){
+               return;
+           }
+
+           if(addMoney < 0){
+                return;
+           }
 
            const balance = getTextFieldValueById('main-balance');
 
@@ -87,6 +109,14 @@ document.getElementById('btn-add-money')
 
            const addMoney = getInputFieldValueById('feni-input-add-money');
 
+           if(isNaN(addMoney)){
+                return;
+            }
+ 
+            if(addMoney < 0){
+                 return;
+            }
+
            const balance = getTextFieldValueById('main-balance');
 
            const newBalance = balance - addMoney;
@@ -100,22 +130,37 @@ document.getElementById('btn-add-money')
 
            const addMoney = getInputFieldValueById('quota-input-add-money');
 
+           if(isNaN(addMoney)){
+                return;
+            }
+ 
+            if(addMoney < 0){
+                 return;
+            }
+
            const balance = getTextFieldValueById('main-balance');
 
            const newBalance = balance - addMoney;
 
            document.getElementById('main-balance').innerText = newBalance + ' ' + 'BDT';
    });
+// Ending of Functions for showing the main balance
 
 
+
+
+// Function for showing Congrats message
    document.getElementById('close-button')
    .addEventListener('click', function(){
            event.preventDefault();
-
-         document.getElementById('cong-message').classList.add("hidden");
+           
+           document.getElementById('cong-message').classList.add("hidden");
    });
 
    
+
+
+// Function for going into blog page 
    document.getElementById('blog-button')
    .addEventListener('click',function(){
          event.preventDefault();
